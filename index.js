b46658e52f60662da469d24261e91e00ee2c3fbc
DUNG NGUYEN TIEN <DUNG.NT181059@sis.hust.edu.vn>
2021-10-30 18:22:56 +0700

    sua file

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello!')
     var b = 1000
     var a = 20002
     var c = a + b
     var dev = "Sontung dep zai"
+    var val2 ="Sua lan 2"
 })
 
 app.listen(port, () => {
