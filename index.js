a520500855608969c83bda98f0e20c5b93889ee4
tienanh_abc <tienanh1472000@gmail.com>
2021-10-30 18:29:16 +0700

    tien anh sua code

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/trang-chu', (req, res) => {
     var b = 1000
     var a = 20002
     var c = a + b
-    var dev = "Sontung dep zai"
+    var dev = "Sontung dep zai okkkkk"
+    var tienanh = "tienanh dang code"
+    if (dev.includes("dep zai")) {
+        res.send('dep trai!')
+    } else {
+        res.send('Hello moi nguoi!')
+    }
 })
 
 app.listen(port, () => {
